251144b33f6b0d9c48962495bff926168b442457
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-09-03 18:53:28 +0200

    refactor article_detail_page.dart to remove unused theme variable

--- lib/features/news/presentation/pages/article_detail_page.dart
+++ lib/features/news/presentation/pages/article_detail_page.dart
@@ -16,8 +16,6 @@ class ArticleDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
     return Scaffold(
       backgroundColor: AppTheme.backgroundLight,
       body: CustomScrollView(
